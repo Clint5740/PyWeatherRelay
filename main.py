2ac5015e993b3c1cd91d27b845383ddb0af87c37
W4LLST0NE <walkermaxeum@gmail.com>
2022-05-19 11:41:06 -0400

    1: Counters added to prevent saying the same warning twice. Manual play mode coming soon(TM) 2: Warnings are only used if they are new or updates. They will not announce ANY cancellations. 3: Names have been simplified into "alert#.wav" instead of individual alerts. It's just easier. I do suggest saving these in a different folder if you intend not to overwrite. 4: INFINITY LOOP! It will ACTUALLY go on for as long as it runs now. This is due to a while loop function - so you have to manually stop it if you don't want it to continue announcing. 5: Instead of annoucing the entire alert, it will print the alert, and then it will annouce the alert in "Warning for Port ST Lucie, GA" format.
    This is overall a much better system and relies on the run console instead of using the python editor. It makes it so much more efficient - and also allows those with 0 coding experience to boot it in python and call it a day.
    It is still suggested to set your own user agent, but it is using a temporary email for now. This email is maintained exclusively for issues and suggestions.
    pyoeas@gmail.com

--- main.py
+++ main.py
@@ -1,35 +1,53 @@
+from nwsapy import nwsapy  # NWS API, do not touch! Allows Python to interact with the National Weather Service's API.
+# NWSAPy is only used for data stream. It is an extension / module. Created by Brandon Molyneaux on GitHub.
+from gtts import gTTS  # Text 2 Speech module. Uses Google API. Requires you to remove things marked.
+# gTTS is used for Text-To-Speech synthesis. Created by Pierre Nicolas Durette on GitHub.
+from playsound import playsound  # Play audio module, so you don't need to manually play it.
+# playsound is pretty self-explanatory. Created by Taylor Marks on GitHub.
+import time
+import os.path
 
-from gtts import gTTS # Text 2 Speech module. Uses Google API. Requires you to remove things marked.
-from playsound import playsound # Play audio module so you don't need to manually play it.
-# Only remove playsound if you don't want automated messages.
-
-import time # this saves me from having to do time.sleep.dingus.penguin
-def wait(t):
+def wait(t):  # I'm more suited for Lua, so this saves me from having to delete and retype stuff.
     time.sleep(t)
 
-def sound():
-    playsound(str(warning.areaDesc) + " " + str(warning.event) + ".wav")  # Remove if playsound is removed
+def filestart():
+    playsound("alert" + str(WC) + ".wav")
 
-# CHANGE THESE VARIABLES IF YOU NEED TO
-Refresh = 180 # How many seconds do you want between announcements?
+nwsapy.set_user_agent("W4LLST0NE", "pyoeas@gmail.com")  # API Keywords to access it. Can work without it.
 
-from nwsapy import nwsapy # NWS API, do not touch!
+# This is where you will change the types of warnings.
+print("Please type the warning types you are looking to annouce.")
+print("Please seperate by comma, and capitalize the first letter of each type.")
+print("If the type selected is invalid, please refresh the script.")
+print("Example : Tornado Warning, Severe Thunderstorm Warning ")
+warningtype = input("")
+# Categories can be found in file types.txt
 
+warnging = nwsapy.get_active_alerts(event=[warningtype])
 
-nwsapy.set_user_agent("Name", "capybaradaddy@gmail.com") # REQUIRED to enter the API.
-while 1 == 1:
-    wait(5) # Just so it doesn't crash. Checks for anything every 5 seconds.
-    Severe = nwsapy.get_active_alerts(event = ["Tornado Warning", "Severe Thunderstorm Warning", "Extreme Wind Warning", "High Wind Warning",
-                                        "Tsunami Warning","Flash Flood Warning"]) # General bad stuff.
-    Tornadic =  nwsapy.get_active_alerts(event = ["Tornado Warning", "Tornado Watch", "Severe Thunderstorm Warning"]) # Spinny stuff.
-    Snow = nwsapy.get_active_alerts(event = ["Blizzard Warning", "Snow Squall Warning", "Winter Storm Warning", "Winter Storm Watch", "Ice Storm Warning", "Freeze Warning", "Wind Chill Warning" ]) # Snow-centric Warnings
-    Flooding = nwsapy.get_active_alerts(event = ["Flood Warning", "Flash Flood Warning"]) # Non-Tornadic/Severe Warnings
-    Fire = nwsapy.get_active_alerts(event = ["Red Flag Warning", "Fire Weather Watch"]) # Fire
-    for warning in Tornadic: # Change "Tornadic" to the event types of your choosing, listed above.
-        print(warning.event + " for..." + warning.areaDesc + " Until")
-        print(warning.headline)
-        tts = gTTS(warning.event + " for..." + warning.areaDesc + " Until") # Remove if gTTS is removed
+warningcount = 0
+if warnging:
+    warningcount = 1
 
-        tts.save(str(warning.areaDesc) + " " + str(warning.event) + ".wav") # Remove if gTTS is remove
-        wait(1) # Will continue to annouce every 3 minutes. Time based in seconds, feel free to change.
-        sound() # Remove if you don't want TTS to play.
+# Hydrologic Outlook
+types = "Alert or Update"
+
+def run():
+    global warningcount
+    global WC
+    WC = 0
+    while warningcount != 0:
+        warningcount = 0
+        if nwsapy.get_active_alerts(type=types):
+            if nwsapy.get_active_alerts(event=[warningtype]):
+                for warning in warnging:
+                    warningcount += 1
+                    WC += 1
+                    print(warning.headline)
+                    tts = gTTS(warning.event + " for..." + warning.areaDesc)
+                    tts.save("alert" + str(WC) + ".wav")  # Saves the .wav file for playback.
+                    filestart()
+                    wait(0.2)
+                    warningcount -= 1
+while 1==1:
+    run()
